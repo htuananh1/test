4d44d30b929482960fd921d2d6def5122b256ede
htuananh1 <83439368+htuananh1@users.noreply.github.com>
2023-06-26 10:38:35 +0700

    Update Anh.lua

--- Anh.lua
+++ Anh.lua
@@ -36,30 +36,34 @@ end
 
 --Script By Blaxken
 
-function getplrsname() for i,v in pairs(game:GetChildren()) do if v.ClassName == "Players" then return v.Name end end end local players = getplrsname() local plr = game[players].LocalPlayer coroutine.resume(coroutine.create(function() while wait(1) do coroutine.resume(coroutine.create(function() for _,v in pairs(game[players]:GetPlayers()) do if v.Name ~= plr.Name and v.Character then v.Character.RightUpperLeg.CanCollide = false v.Character.RightUpperLeg.Transparency = 75 v.Character.RightUpperLeg.Size = Vector3.new(21,21,21) v.Character.LeftUpperLeg.CanCollide = false v.Character.LeftUpperLeg.Transparency = 75 v.Character.LeftUpperLeg.Size = Vector3.new(21,21,21) v.Character.HeadHB.CanCollide = false v.Character.HeadHB.Transparency = 75 v.Character.HeadHB.Size = Vector3.new(21,21,21) v.Character.HumanoidRootPart.CanCollide = false v.Character.HumanoidRootPart.Transparency = 75 v.Character.HumanoidRootPart.Size = Vector3.new(21,21,21) end end end)) end end))
-
---Made by Andrheyplayz_officalyt
-local gui = Instance.new("BillboardGui");
-gui.Name = "";
-gui.AlwaysOnTop = true;
-gui.LightInfluence = 0;
-gui.Size = UDim2.new(1.75, 0, 1.75, 0);
-local frame = Instance.new("Frame", gui);
-frame.BackgroundColor3 = Color3.fromRGB(170, 0, 0);
-frame.Size = UDim2.new(1, 0, 1, 0);
-frame.BorderSizePixel = 4;
-frame.BorderColor3 = Color3.fromRGB(0, 0, 0);
-local gi = gui:Clone();
-local body = frame:Clone();
-body.Parent = gi;
-body.BackgroundColor3 = Color3.fromRGB(0, 170, 170);
-
-for _, v in pairs(game:GetService("Players"):GetPlayers()) do
-    if v.Name ~= game:GetService("Players").LocalPlayer.Name and v.Character and v.Character:FindFirstChild("Head") then
-        gui:Clone().Parent = v.Character.Head;
-    end
+function CreateSG(name,parent,face)
+local SurfaceGui = Instance.new("SurfaceGui",parent)
+SurfaceGui.Parent = parent
+SurfaceGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+SurfaceGui.Face = Enum.NormalId[face]
+SurfaceGui.LightInfluence = 0
+SurfaceGui.ResetOnSpawn = false
+SurfaceGui.Name = name
+SurfaceGui.AlwaysOnTop = true
+local Frame = Instance.new("Frame",SurfaceGui)
+Frame.BackgroundColor3 = Color3.fromRGB(85, 170, 255)
+Frame.Size = UDim2.new(1,0,1,0)
 end
-
-local c = 1 function zigzag(X)  return math.acos(math.cos(X * math.pi)) / math.pi end game:GetService("RunService").RenderStepped:Connect(function()  if game.Workspace.Camera:FindFirstChild('Arms') then   for i,v in pairs(game.Workspace.Camera.Arms:GetDescendants()) do    if v.ClassName == 'MeshPart' then      v.Color = Color3.fromHSV(zigzag(c),1,1)     c = c + .0001    end   end  end end)
-net = true -- if false = do nothing
-notify = true -- set this to false if u don't want to see notiflication
+while wait(1) do
+for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+if v ~= game:GetService("Players").LocalPlayer and v.Character ~= nil and
+v.Character:FindFirstChild("LowerTorso") and v.Character.LowerTorso:FindFirstChild("cham") == nil then
+for i,v in pairs (v.Character:GetChildren()) do
+if v:IsA("MeshPart") or v.Name == "LowerTorso" then
+CreateSG("cham",v,"Back")
+CreateSG("cham",v,"Front")
+CreateSG("cham",v,"Left")
+CreateSG("cham",v,"Right")
+CreateSG("cham",v,"Right")
+CreateSG("cham",v,"Top")
+CreateSG("cham",v,"Bottom")
+end
+end
+end
+end
+end
