cf2b57ab052cb5019bb9b46390b14435745b5d4e
htuananh1 <83439368+htuananh1@users.noreply.github.com>
2025-05-30 11:29:55 +0700

    Update and rename Bypass to video.js

---
+++ video.js
@@ -0,0 +1,8 @@
+export const videoLinks = [
+  "https://v1.pinimg.com/videos/iht/expMp4/a7/9b/2d/a79b2df8b2fefbbcc3a5ec0d32e0ccc0_720w.mp4",
+  "https://v1.pinimg.com/videos/iht/expMp4/ca/f9/c0/caf9c066b9adee71afeed8de8b8cc18c_720w.mp4",
+  "https://v1.pinimg.com/videos/iht/expMp4/f1/e7/69/f1e7697c421077b43f931265ba1b717e_720w.mp4",
+  "https://v1.pinimg.com/videos/iht/720p/72/f2/b2/72f2b2b305ef9fb941d1b05e07793066.mp4",
+  "https://v1.pinimg.com/videos/mc/720p/91/77/47/917747d4ae76d3a9c5d9e727b5f6080a.mp4",
+  "https://v1.pinimg.com/videos/mc/720p/65/d7/97/65d797f38c7af9a9b4b744b0f5e9f829.mp4"
+];
